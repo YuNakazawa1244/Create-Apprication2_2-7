69cd18267be386adc589bf7a7605f77e5481bbef
YuNakazawa1244 <g_o_w_yu321903@yahoo.ne.jp>
2021-03-15 13:56:57 +0000

    [Add]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e2e20c6fb649365ce3100116b60e8405d5a430f455aba24d2d3a66cdb39a12e24ba0256fe55d470d64af455ec4077da6d914ce74995d28dd284f984e05f95192'
