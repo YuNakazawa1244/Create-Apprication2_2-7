9f0d79865216892cc9fd78ee88567dbeb77405ba
YuNakazawa1244 <g_o_w_yu321903@yahoo.ne.jp>
2021-03-16 04:27:05 +0000

    [Finish]5-4

---
+++ app/models/post_commnet.rb
@@ -0,0 +1,7 @@
+class PostCommnet < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :post_image
+  belongs_to :post_image
+
+end
---
+++ db/migrate/20210316015040_create_post_commnets.rb
@@ -0,0 +1,10 @@
+class CreatePostCommnets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :post_commnets do |t|
+      t.text :comment
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
